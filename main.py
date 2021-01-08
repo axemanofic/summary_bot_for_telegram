58f7d23e6d7513db5ac1f3c3bd38b022160dd4f0
axemanofic <axeman.ofic@gmail.com>
2021-01-08 20:21:22 +0300

    add simple command open admin panel

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 import sys
 import time
 import config
@@ -12,21 +11,51 @@ bot = telebot.TeleBot(token=config.TOKEN)
 
 
 def getReplyKeyboard(text):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=2)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True,
+                                         one_time_keyboard=True)
     keyboard.add(types.KeyboardButton(text))
-    types.KeyboardButton
     return keyboard
+
+
+def getInlineKeyboard():
+    keyboard = types.InlineKeyboardMarkup()
+    button1 = types.InlineKeyboardButton('О себе', callback_data='self')
+    button2 = types.InlineKeyboardButton('Скиллы', callback_data='skills')
+    button3 = types.InlineKeyboardButton('Образование',
+                                         callback_data='certificates')
+    keyboard.row(button1, button2)
+    return keyboard
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == 'self':
+        bot.edit_message_text('о себе', chat_id=call.message.chat.id,
+                              message_id=call.message.id, reply_markup=getInlineKeyboard())
+    elif call.data == 'skills':
+        bot.edit_message_text('скиллы', chat_id=call.message.chat.id,
+                              message_id=call.message.id, reply_markup=getInlineKeyboard())
+    elif call.data == 'certificates':
+        bot.edit_message_text('сертификаты', chat_id=call.message.chat.id,
+                              message_id=call.message.id, reply_markup=getInlineKeyboard())
 
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
-    bot.send_message(message.chat.id, "Привет 😊",
-        reply_markup=getReplyKeyboard("Просмотреть резюме 😉"))
+    bot.send_message(message.chat.id, 'Привет 😊',
+                     reply_markup=getReplyKeyboard('Просмотреть резюме 📚'))
 
 
 @bot.message_handler(content_types=['text'])
 def text_handler(message):
-    bot.send_message(message.chat.id, message.text)
+    print(message.text, message.from_user.id)
+
+    if message.text == 'Просмотреть резюме 📚':
+        bot.send_message(message.chat.id, 'Выбирай',
+                         reply_markup=getInlineKeyboard())
+    elif (message.text == 'admin') and (message.chat.id == config.MY_ID):
+        bot.send_message(message.chat.id,
+                         'Добро пожаловать {}'.format(message.chat.username))
 
 
 def main_loop():
